25c9b4cd671081a1185a8f2a4cbca817d32f6999
doran1534 <69667804+doran1534@users.noreply.github.com>
2020-09-18 18:05:05 +0900

    Update BookDetailService.java

--- src/kr/co/sist/admin/book/service/BookDetailService.java
+++ src/kr/co/sist/admin/book/service/BookDetailService.java
@@ -1,5 +1,35 @@
 package kr.co.sist.admin.book.service;
 
-public class BookDetailService {
+import kr.co.sist.admin.book.domain.BookDetailDomain;
+import kr.co.sist.admin.book.vo.BookModifyVO;
 
+public class BookDetailService {
+	/**
+	 * 도서를 추가하는 일
+	 * @param bmVO
+	 */
+	public void addBook(BookModifyVO bmVO) {
+		
+	}
+	
+	/**
+	 * 도서정보를 변경하는 일
+	 * @param bmVO
+	 * @return
+	 */
+	public int changeBook(BookModifyVO bmVO) {
+		int result = 0;
+		return result;
+	}
+	
+	/**
+	 * 도서의 isbn을 받고 해당 도서의 상세정보를 보여주는 일
+	 * @param book_isbn
+	 * @return
+	 */
+	public BookDetailDomain selectBookDetail(String book_isbn) {
+		BookDetailDomain bdd = null;
+		
+		return bdd;
+	}
 }
