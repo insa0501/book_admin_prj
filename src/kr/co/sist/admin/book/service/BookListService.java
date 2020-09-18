b90686a967f2ade3592056e2f5fe11eb15ca0f47
doran1534 <69667804+doran1534@users.noreply.github.com>
2020-09-18 18:02:25 +0900

    Update BookListService.java

--- src/kr/co/sist/admin/book/service/BookListService.java
+++ src/kr/co/sist/admin/book/service/BookListService.java
@@ -1,5 +1,85 @@
 package kr.co.sist.admin.book.service;
 
-public class BookListService {
+import java.util.ArrayList;
+import java.util.List;
 
+import kr.co.sist.admin.book.domain.BookListDomain;
+import kr.co.sist.admin.book.domain.PageNationDomain;
+import kr.co.sist.admin.book.vo.BookRangeVO;
+import kr.co.sist.admin.book.vo.SelectDataVO;
+
+public class BookListService {
+	/**
+	 * 도서 리스트를 얻는 일
+	 * @param brVO
+	 * @param sdVO
+	 * @return
+	 */
+	public List<BookListDomain> selectBookList(BookRangeVO brVO, SelectDataVO sdVO) {
+		List<BookListDomain> list = new ArrayList<BookListDomain>();
+		return list;
+	}//selectBookList
+	
+	/**
+	 * 하단에 페이지를 나누는 일
+	 * @param pnd
+	 * @return
+	 */
+	public String pageNation(PageNationDomain pnd) {
+		String pageNation = "";
+		return pageNation;
+	}//pageNation
+	
+	/**
+	 * 도서의 전체 갯수
+	 * @return
+	 */
+	public int totalCount() {
+		int totalCount = 0;
+		
+		return totalCount;
+	}//totalCount
+	
+	/**
+	 * 보여줄 도서의 갯수
+	 * @return
+	 */
+	public int pageScale() {
+		int pageScale = 10;
+		return pageScale;
+	}
+	
+	/**
+	 * 보여줄 갯수
+	 * @param totalCount
+	 * @param pageScale
+	 * @return
+	 */
+	public int totalPage(int totalCount, int pageScale) {
+		int totalPage=(int)Math.ceil((double)totalCount/pageScale);
+		
+		return totalPage;
+	}
+	
+	/**
+	 * 현재페이지에 보여줄 시작 번호
+	 * @param currentPage
+	 * @param pageScale
+	 * @return
+	 */
+	public int startNum(int currentPage, int pageScale) {
+		int startNum= currentPage*pageScale-pageScale+1;
+		return startNum;
+	}
+	
+	/**
+	 * 현재페이지에 보여줄 마지막 번호
+	 * @param startNum
+	 * @param pageScale
+	 * @return
+	 */
+	public int endNum(int startNum, int pageScale) {
+		int endNum = startNum+pageScale-1;
+		return endNum;
+	}
 }
